bba7c63031dfa861c139efa072397afc6c926c67
Liu Xiaodong <xiaodong.liu@intel.com>
2020-04-01 01:35:44 -0400

    edriven: applied in bdev_aio
    Change-Id: I1c38951f4feef2beacb1e3a2272fcaeda097b820

--- module/bdev/aio/bdev_aio.c
+++ module/bdev/aio/bdev_aio.c
@@ -161,6 +161,14 @@ bdev_aio_close(struct file_disk *disk)
 	return 0;
 }
 
+// CHANGE: edriven
+static inline bdev_aio_request_edriven(struct iocb *iocb, struct bdev_aio_io_channel *aio_ch)
+{
+	int aio_efd = spdk_edriven_source_get_efd(spdk_poller_to_edriven_source(aio_ch->group_ch->poller));
+
+	io_set_eventfd(iocb, aio_efd);
+}
+
 static int64_t
 bdev_aio_readv(struct file_disk *fdisk, struct spdk_io_channel *ch,
 	       struct bdev_aio_task *aio_task,
@@ -171,6 +179,7 @@ bdev_aio_readv(struct file_disk *fdisk, struct spdk_io_channel *ch,
 	int rc;
 
 	io_prep_preadv(iocb, fdisk->fd, iov, iovcnt, offset);
+	bdev_aio_request_edriven(iocb, aio_ch);
 	iocb->data = aio_task;
 	aio_task->len = nbytes;
 	aio_task->ch = aio_ch;
@@ -202,6 +211,7 @@ bdev_aio_writev(struct file_disk *fdisk, struct spdk_io_channel *ch,
 	int rc;
 
 	io_prep_pwritev(iocb, fdisk->fd, iov, iovcnt, offset);
+	bdev_aio_request_edriven(iocb, aio_ch);
 	iocb->data = aio_task;
 	aio_task->len = len;
 	aio_task->ch = aio_ch;
@@ -248,6 +258,7 @@ bdev_aio_destruct(void *ctx)
 	return rc;
 }
 
+// TODO: edriven
 static int
 bdev_user_io_getevents(io_context_t io_ctx, unsigned int max, struct io_event *uevents)
 {
@@ -311,6 +322,10 @@ bdev_user_io_getevents(io_context_t io_ctx, unsigned int max, struct io_event *u
 	return count;
 }
 
+// TODO: edriven
+// efd for aio poll is EPOLLET, so if completed IO number is SPDK_AIO_QUEUE_DEPTH,
+// io_getevent should be called again to ensure all io are completed.
+// whether to use "read(efd, &finished_aio, sizeof(finished_aio)"
 static int
 bdev_aio_group_poll(void *arg)
 {
@@ -556,6 +571,7 @@ static void aio_free_disk(struct file_disk *fdisk)
 	free(fdisk);
 }
 
+// CHANGE: edriven
 static int
 bdev_aio_group_create_cb(void *io_device, void *ctx_buf)
 {
@@ -566,10 +582,13 @@ bdev_aio_group_create_cb(void *io_device, void *ctx_buf)
 		return -1;
 	}
 
-	ch->poller = spdk_poller_register(bdev_aio_group_poll, ch, 0);
+	//ch->poller = spdk_poller_register(bdev_aio_group_poll, ch, 0);
+	ch->poller = spdk_thread_edriven_register(bdev_aio_group_poll, ch, NULL);
+
 	return 0;
 }
 
+// CHANGE: edriven
 static void
 bdev_aio_group_destroy_cb(void *io_device, void *ctx_buf)
 {
@@ -577,7 +596,8 @@ bdev_aio_group_destroy_cb(void *io_device, void *ctx_buf)
 
 	io_destroy(ch->io_ctx);
 
-	spdk_poller_unregister(&ch->poller);
+	spdk_thread_edriven_unregister(&spdk_poller_to_edriven_source(ch->poller));
+	//spdk_poller_unregister(&ch->poller);
 }
 
 int
